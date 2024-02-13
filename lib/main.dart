4560fd227930f94b6d4d899c59cd132ae4074372
peteragurto <130622718+peteragurto@users.noreply.github.com>
2024-02-12 22:59:03 -0500

    Cambios para registro con Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: "Flutter Demo",
-    theme: ThemeData(primarySwatch: Colors.green),
+    theme: ThemeData(primarySwatch: Colors.green, useMaterial3: true),
     home: const HomePage(),
   ));
 }
@@ -40,22 +44,50 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.amber,
         title: const Text("Registro"),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-            decoration: const InputDecoration(hintText: "Introduce tu email"),
-          ),
-          TextField(
-            controller: _password,
-            decoration:
-                const InputDecoration(hintText: "Introduce tu contraseña"),
-          ),
-          TextButton(
-            onPressed: () async {},
-            child: const Text("Registrarse"),
-          ),
-        ],
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Column(
+                children: [
+                  TextField(
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      hintText: "Introduce tu email",
+                    ),
+                  ),
+                  TextField(
+                    controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                        hintText: "Introduce tu contraseña"),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      final email = _email.text;
+                      final password = _password.text;
+                      final userCredential = await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: email, password: password);
+
+                      print(userCredential);
+                    },
+                    child: const Text("Registrarse"),
+                  ),
+                ],
+              );
+            default:
+              return const Text("Cargando");
+          }
+        },
       ),
     );
   }
