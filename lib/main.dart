0fac6a1c140de0240b681f7674c2500984b632b2
peteragurto <130622718+peteragurto@users.noreply.github.com>
2024-02-18 01:37:55 -0500

    Navegación exitosa entre vistas - prototipo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intro_flutter/firebase_options.dart';
 import 'package:intro_flutter/views/login_view.dart';
 import 'package:intro_flutter/views/register_view.dart';
+import 'package:intro_flutter/views/verify_emailview.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,13 +31,25 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            /*final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
+            final user = FirebaseAuth.instance.currentUser;
+            print(user != null);
+            print(user?.emailVerified);
+            if (user != null) {
+              // Forzar la actualización de la información del usuario
+              if (user.emailVerified) {
+                print("Está verificado");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            /*if (user?.emailVerified ?? false) {
                 return const Text("Listo");
               } else {
                 return const VerifyEmailView();
               }*/
-            return const LoginView();
+            return const Text("Hecho");
           default:
             return const CircularProgressIndicator();
         }
