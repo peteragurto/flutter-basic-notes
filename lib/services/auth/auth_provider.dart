85f713aaa107fc12a77497484e26fe056222fc78
peteragurto <130622718+peteragurto@users.noreply.github.com>
2024-02-23 14:24:23 -0500

    Creados servicios proveedores de autentificación(Firebase Provider y Auth Provider)

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,19 @@
+import 'package:intro_flutter/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+
+  Future<void> signOut();
+
+  Future<void> sendEmailVerification();
+}
