5a30cf06df66b07cc3cd46a9c332e5a466e283f9
peteragurto <130622718+peteragurto@users.noreply.github.com>
2024-02-27 23:59:09 -0500

    Test de Auth

---
+++ test/auth_test.dart
@@ -0,0 +1,67 @@
+import "package:intro_flutter/services/auth/auth_exceptions.dart";
+import "package:intro_flutter/services/auth/auth_provider.dart";
+import "package:intro_flutter/services/auth/auth_user.dart";
+import "package:test/test.dart";
+
+void main() {}
+
+class IsNotInitializedException implements Exception {}
+
+class MockAuthProvider implements AuthProvider {
+  AuthUser? _user;
+  var _isInitialized = false;
+  bool get isInitialized => _isInitialized;
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) async {
+    if (!isInitialized) throw IsNotInitializedException();
+    await Future.delayed(const Duration(seconds: 1));
+    return logIn(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  AuthUser? get currentUser => _user;
+
+  @override
+  Future<void> initialize() async {
+    await Future.delayed(const Duration(seconds: 1));
+    _isInitialized = true;
+  }
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) {
+    if (!isInitialized) throw IsNotInitializedException();
+    if (email == "pedroagurto@gmail.com") throw UserNotFoundAuthException();
+    if (password == "trululu") throw WrongPasswordAuthException();
+    const user = AuthUser(isEmailVerified: false);
+    _user = user;
+    return Future.value(user);
+  }
+
+  @override
+  Future<void> sendEmailVerification() async {
+    if (!isInitialized) throw IsNotInitializedException();
+    final user = _user;
+    if (user == null) throw UserNotFoundAuthException();
+    const newUser = AuthUser(isEmailVerified: true);
+    _user = newUser;
+  }
+
+  @override
+  Future<void> signOut() async {
+    if (!isInitialized) throw IsNotInitializedException();
+    final user = _user;
+    if (user == null) throw UserNotFoundAuthException();
+    const newUser = AuthUser(isEmailVerified: true);
+    _user = newUser;
+  }
+}
