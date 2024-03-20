536c75ad77a9d7c16da658a96000f73b38538271
peteragurto <130622718+peteragurto@users.noreply.github.com>
2024-03-20 18:46:57 -0500

    Agregando eventos y estados extras

--- lib/services/auth/bloc/auth_event.dart
+++ lib/services/auth/bloc/auth_event.dart
@@ -9,10 +9,26 @@ class AuthEventInitialize extends AuthEvent {
   const AuthEventInitialize();
 }
 
+//Eventos de logIn
+
+class AuthEventRegister extends AuthEvent {
+  final String email;
+  final String password;
+  const AuthEventRegister(this.email, this.password);
+}
+
+class AuthEventShouldRegister extends AuthEvent {
+  const AuthEventShouldRegister();
+}
+
 class AuthEventLogIn extends AuthEvent {
   final String email;
   final String password;
   const AuthEventLogIn(this.email, this.password);
+}
+
+class AuthEventSendEmailVerification extends AuthEvent {
+  const AuthEventSendEmailVerification();
 }
 
 class AuthEventLogOut extends AuthEvent {
